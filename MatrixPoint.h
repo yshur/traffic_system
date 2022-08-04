a8196444455b7912d0e0c1c333b9c083d619069a
Yair Shur <yairs@pentenetworks.com>
2022-08-04 09:55:40 +0300

    update some variables

---
+++ MatrixPoint.h
@@ -0,0 +1,18 @@
+#ifndef __MATRIX_POINT_H
+#define __MATRIX_POINT_H
+
+class Point;
+class Street;
+
+class MatrixPoint
+{
+private:
+    const int m_hight;
+    const int m_width;
+    Point **m_matrix;
+    Street *m_streets;
+
+public:
+}
+
+#endif __MATRIX_POINT_H
--- TrafficSystem.h
+++ TrafficSystem.h
@@ -1,1 +1,4 @@
+#ifndef __TRAFFIC_SYSTEM_H
+#define __TRAFFIC_SYSTEM_H
 
+#endif __TRAFFIC_SYSTEM_H
